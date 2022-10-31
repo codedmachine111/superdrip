bfaebeef1c2e7b9f16dd4c921c95ff7213d40844
codedmachine111 <charlesbabbage1709@gmail.com>
2022-10-31 13:00:45 +0530

    Added redux-persist to save data to localStorage

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import {store} from './store/store';
-import {Provider} from 'react-redux';
+import { store, persistor } from "./store/store";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </Router>
+    <PersistGate loading={null} persistor={persistor}>
+      <Router>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </Router>
+    </PersistGate>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,19 @@
 import {compose, applyMiddleware, createStore} from 'redux';
 import logger from 'redux-logger';
-
+import { persistStore, persistReducer} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import {rootReducer} from './root-reducer';
 
-const middlewares = [logger];
+const persistConfig = {
+    key: 'root',
+    storage,
+    blackList:['user'],
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
 const composedEnhancers = compose(applyMiddleware(...middlewares));
 
-export const store = createStore(rootReducer,undefined,composedEnhancers);
+export const store = createStore(persistedReducer,undefined,composedEnhancers);
+
+export const persistor = persistStore(store);
